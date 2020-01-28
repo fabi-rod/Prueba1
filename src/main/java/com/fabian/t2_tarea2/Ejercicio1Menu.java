ecee7369a4293ccb3518faf7e58785bf8236bede
fabi <fabian@gmail.com>
2020-01-28 16:46:09 +0100

    Se ha añadido mensaje de prueba 4

--- src/main/java/com/fabian/t2_tarea2/Ejercicio1Menu.java
+++ src/main/java/com/fabian/t2_tarea2/Ejercicio1Menu.java
@@ -32,6 +32,7 @@ public class Ejercicio1Menu {
         System.out.println("Prueba GIT");
         System.out.println("Prueba GIT 2");
         System.out.println("Prueba GIT 3");
+        System.out.println("4");
 
         int valorNumerico;      //Valor a introducir que entrará en el módulo correspondiente
         int valorVacio = 0;     //Valor a que se usará para salir del programa
